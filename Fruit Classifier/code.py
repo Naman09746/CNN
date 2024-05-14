2efbe2c89794d4f0c92ffa8f23c6f76092fffb48
Naman Joshi <163289600+Naman09746@users.noreply.github.com>
2024-05-14 22:23:37 +0530

    Update code.py

--- Fruit Classifier/code.py
+++ Fruit Classifier/code.py
@@ -3,3 +3,13 @@ import random
 import pandas as pd
 import numpy as np
 import torchvision
+import torch
+from torch import nn
+from torch.autograd import Variable
+from sklearn.model_selection import train_test_split
+import torchvision.transforms as T
+from torchvision.datasets import ImageFolder
+import matplotlib.pyplot as plt
+import cv2
+from torch.utils.data import DataLoader
+from sklearn.metrics import accuracy_score
