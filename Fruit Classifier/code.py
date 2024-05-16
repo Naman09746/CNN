ceed5340f88c74af251c6be1f85918b05deb1816
Naman Joshi <163289600+Naman09746@users.noreply.github.com>
2024-05-16 18:12:12 +0530

    Update code.py

--- Fruit Classifier/code.py
+++ Fruit Classifier/code.py
@@ -13,3 +13,12 @@ import matplotlib.pyplot as plt
 import cv2
 from torch.utils.data import DataLoader
 from sklearn.metrics import accuracy_score
+
+# Loading data
+transforms_train = T.Compose([T.ToTensor(),T.Normalize([0.5,0.5,0.5],[0.5,0.5,0.5])])
+image_data_train = ImageFolder("./fruits-360/Training",transform=transforms_train)
+image_data_test = ImageFolder("./fruits-360/Validation",transform=transforms_train)
+
+# Shuffling data and then collecting all the labels.
+random.shuffle(image_data_train.samples)
+random.shuffle(image_data_test.samples)
