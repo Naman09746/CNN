099703ecc44d844e9222eae2e29399452c2748a6
Naman Joshi <163289600+Naman09746@users.noreply.github.com>
2024-06-10 00:00:17 +0530

    Update code.py

--- Fruit Classifier/code.py
+++ Fruit Classifier/code.py
@@ -33,7 +33,6 @@ classes = len(image_data_train.classes)
 len_train_data = len(image_data_train)
 len_test_data = len(image_data_test)
 
-
 def get_labels():
     labels_train = [] # All the labels
     labels_test = []
@@ -46,3 +45,70 @@ def get_labels():
     return (labels_train, labels_test)
 
 labels_train, labels_test = get_labels()
+
+
+
+train_loader = DataLoader(dataset=image_data_train,batch_size=100)
+test_loader = DataLoader(dataset=image_data_test,batch_size=100)
+
+print (iter(train_loader).next()[0].shape)
+record = iter(train_loader).next()[0]
+
+# Flatten Layer
+class Flatten(nn.Module):
+    def forward(self, x):
+        return x.view(x.size(0), -1)
+# Main Model
+class Model:
+    def build_model(self):
+        model = nn.Sequential(nn.Conv2d(3, 64, kernel_size=5, stride=1), nn.ReLU(),nn.MaxPool2d(2), nn.Conv2d(64, 64, kernel_size=7, stride=1),
+                             nn.ReLU(), nn.MaxPool2d(3), nn.Conv2d(64, 64, kernel_size=7), nn.ReLU(), nn.MaxPool2d(5), Flatten(), nn.Linear(64, 100), nn.ReLU(), nn.Linear(100, 65))
+        return model
+
+# Building 
+model = Model()
+model = model.build_model()
+
+# Checking for GPU
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+model.to(device)
+
+print (model)
+print (labels_train[0])
+print (image_data_train.samples[0][1])
+
+optimizer = torch.optim.Adagrad(model.parameters(), lr=0.01)
+criterion = nn.CrossEntropyLoss()
+
+# Train the model
+def train(epochs):
+    model.train()
+    losses = []
+    for epoch in range(1, epochs+1):
+        print ("epoch #", epoch)
+        current_loss = 0.0
+        for feature, label in train_loader:
+            x = Variable(feature, requires_grad=False).float().to(device)
+            y = Variable(label, requires_grad=False).long().to(device)
+            optimizer.zero_grad() # Zeroing the grads
+            y_pred = model(x) # Calculating prediction
+            correct = y_pred.max(1)[1].eq(y).sum()
+            print ("no. of correct items classified: ", correct.item())
+            loss = criterion(y_pred, y) # Calculating loss (log_softmax already included)
+            print ("loss: ", loss.item())
+            current_loss+=loss.item()
+            loss.backward() # Gradient cal
+            optimizer.step() # Changing weights
+        losses.append(current_loss) # Only storing loss after every epoch
+    return losses
+# Test the model
+def test():
+    model.eval()
+    with torch.no_grad():
+        for feature, label in test_loader:
+            pred = model(feature)
+            print ("acc: ", accuracy_score(labels_test, pred.max(1)[1].data.numpy()) * 100)
+            loss = criterion(pred, label)
+            print ("loss: ", loss.item())
+
+
